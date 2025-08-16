a4e3226956a424455880d21806f30278500c95af
sadia123kona <jinatimami964@gmail.com>
2025-08-17 00:29:47 +0600

    kona

---
+++ feedback1.php
@@ -0,0 +1,20 @@
+<?php
+
+$con = mysqli_connect('localhost', 'root', '', 'travel');
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$feedback = $_POST['feedbk'];
+
+$que="INSERT INTO `feedback` (name,email,feedbk) VALUES ('$name','$email','$feedback')";
+
+$result = mysqli_query($con, $que);
+if (mysqli_query($con, $que)) {
+    echo "<h2>Thanks for your opinion</h2>";
+    echo "<a href='feedback.html'>Back to Home</a>";
+} else {
+    echo "Error: " . mysqli_error($con);
+}
+
+
+?>
